831bd5a563e5e37c22c9e0be6f9bb1b5f512fb86
Quantum <quantumwavves@example.com>
2023-04-02 17:42:35 -0600

    minor changes

--- bin/deploys/Query.ps1
+++ bin/deploys/Query.ps1
@@ -3,9 +3,9 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass
 Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/quantumwavves/PoshScripts-qw/master/resources/Executables/gnupg-w32-2.3.8_20221013.exe" -OutFile "$env:temp\gnupg-w32-2.3.8_20221013.exe"
 cmd.exe /c $env:temp\gnupg-w32-2.3.8_20221013.exe /S /V /QN
-Invoke-WebRequest -Uri "https://cutt.ly/xN0inMf" -OutFile "$env:temp\Optimizerv6.ps1.gpg"
-& 'C:\Program Files (x86)\GnuPG\bin\gpg.exe' -d $env:temp\Optimizerv6.ps1.gpg >> $env:temp\Optimizerv6.ps1
-. $env:temp\Optimizerv6.ps1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/quantumwavves/PoshScripts-qw/master/resources/Binary/ov6.gpg" -OutFile "$env:temp\ov6.ps1.gpg"
+& 'C:\Program Files (x86)\GnuPG\bin\gpg.exe' -d $env:temp\ov6.ps1.gpg >> $env:temp\ov6.ps1
+. $env:temp\ov6.ps1
 & 'C:\Program Files (x86)\GnuPG\gnupg-uninstall.exe' /S /V /QN
 Remove-Item $env:appdata\gnupg -Recurse
 Clear-History
--- bin/deploys/QueryQW.ps1
+++ bin/deploys/QueryQW.ps1
@@ -1,9 +1,9 @@
 ##V.0.6
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-Invoke-WebRequest -Uri "https://cutt.ly/UN0iz0z" -OutFile "$env:temp\gnupg-w32-2.3.8_20221013.exe"
+Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/quantumwavves/PoshScripts-qw/master/resources/Executables/gnupg-w32-2.3.8_20221013.exe" -OutFile "$env:temp\gnupg-w32-2.3.8_20221013.exe"
 cmd.exe /c $env:temp\gnupg-w32-2.3.8_20221013.exe /S /V /QN
-Invoke-WebRequest -Uri "https://github.com/quantumwavves/yofukashinouta/raw/master/bin/qwo.ps1.gpg" -OutFile "$env:temp\qwo.ps1.gpg"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/quantumwavves/PoshScripts-qw/master/resources/Binary/qwo.ps1.gpg" -OutFile "$env:temp\qwo.ps1.gpg"
 & 'C:\Program Files (x86)\GnuPG\bin\gpg.exe' -d $env:temp\qwo.ps1.gpg >> $env:temp\qwo.ps1
 . $env:temp\qwo.ps1
 & 'C:\Program Files (x86)\GnuPG\gnupg-uninstall.exe' /S /V /QN
