3ff699d614c8f45d3b912893bcf30a1c0ca4d6c2
Quantum <quantumwavves@gmail.com>
2023-04-02 01:41:05 -0600

    Add MSO2019 deploy

---
+++ bin/deploys/D2019ProPlus.ps1
@@ -0,0 +1,9 @@
+function 2019Deploy {
+    (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16130-20218.exe", "$env:temp\offiedeploy.exe")
+    mkdir $env:temp\deploy
+    .$env:temp\offiedeploy.exe /quiet /extract:$env:temp\deploy
+    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/quantumwavves/PoshScripts-qw/master/resources/OfficeDeploy/2019ProPlus.xml", "$env:temp\deploy\2019ProPlus.xml")
+    .$env:temp\deploy\setup.exe /configure $env:temp\deploy\2019ProPlus.xml
+    Write-Output"FINISH"
+}
+2019Deploy
